4c9d57c84d9f6712e0a0abbc495142967f379a2c
Anirudh Sharma <anirudhsharma.0221@gmail.com>
2024-12-16 21:18:39 +0530

    Signin and signup api routes

---
+++ src/api/UserAPI.ts
@@ -0,0 +1,36 @@
+import api from './configs/axiosConfigs';
+
+const UserAPI = {
+    signup: async (name: string, email: string, password: string) => {
+        const response = await api.request({
+            url: '/signup',
+            method: 'POST',
+            data: {
+                name,
+                email,
+                password
+            }
+        });
+        return response;
+    },
+    signin: async (email: string, password: string) => {
+        const response = await api.request({
+            url: '/signin',
+            method: 'POST',
+            data: {
+                email,
+                password
+            }
+        });
+        return response
+    },
+    verifyMe: async () => {
+        const response = await api.request({
+            url: '/me',
+            method: 'GET',
+        });
+        return response
+    },
+}
+
+export default UserAPI
