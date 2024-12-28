6820cb03d227987efb241067f4371505cb1da902
Anirudh Sharma <anirudhsharma.0221@gmail.com>
2024-12-29 02:24:26 +0530

    feat: added mobile number in signup form

--- src/api/UserAPI.ts
+++ src/api/UserAPI.ts
@@ -1,38 +1,44 @@
-import api from './configs/axiosConfigs';
+import api from "./configs/axiosConfigs";
 
 const UserAPI = {
-    signup: async (name: string, email: string, password: string) => {
-        const response = await api.request({
-            url: '/signup',
-            method: 'POST',
-            data: {
-                name,
-                email,
-                password
-            },
-        });
-        return response;
-    },
-    signin: async (email: string, password: string) => {
-        const response = await api.request({
-            url: '/signin',
-            method: 'POST',
-            data: {
-                email,
-                password
-            },
-            withCredentials: true
-        });
-        return response
-    },
-    verifyMe: async () => {
-        const response = await api.request({
-            url: '/me',
-            method: 'GET',
-            withCredentials: true
-        });
-        return response
-    },
-}
+  signup: async (
+    name: string,
+    email: string,
+    password: string,
+    mobile: string
+  ) => {
+    const response = await api.request({
+      url: "/signup",
+      method: "POST",
+      data: {
+        name,
+        email,
+        password,
+        mobile,
+      },
+    });
+    return response;
+  },
+  signin: async (email: string, password: string) => {
+    const response = await api.request({
+      url: "/signin",
+      method: "POST",
+      data: {
+        email,
+        password,
+      },
+      withCredentials: true,
+    });
+    return response;
+  },
+  verifyMe: async () => {
+    const response = await api.request({
+      url: "/me",
+      method: "GET",
+      withCredentials: true,
+    });
+    return response;
+  },
+};
 
-export default UserAPI
+export default UserAPI;
